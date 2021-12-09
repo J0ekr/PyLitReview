46b0700c7f052273183e3c6671c36d42bf343743
Jan Leusmann <jan.leusmann@gmx.de>
2021-12-09 14:08:59 +0100

    Initial PyLitReview commit

---
+++ config_template.py
@@ -0,0 +1,12 @@
+UNI_MAIL = ""
+UNI_PWD = ""
+UNI_USER = ""
+# Login for ScienceDirect
+ScienceDirect_MAIL = ""
+ScienceDirect_PWD = ""
+
+# downloadfolder should look like: C:\\Users\\User\\Path\\To\\PyLitReview\\\\Library
+# e.g. C:\\Users\\Jan\\Documents\\GitHub\\MA-Thesis\\PyLitReview\\PostReview\\acm
+downloadfolder_acm = ""
+downloadfolder_ieee = ""
+downloadfolder_sd = ""
